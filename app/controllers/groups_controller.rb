c1077a48564f9137ffaf7311fd4fb5608e8477db
takuya12 <bucsojmfa12@gmail.com>
2019-11-26 15:10:53 +0900

    commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,30 +17,21 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
   end
 
-
-  # def edit
-  # end
-
-  # def update
-  # end
-
-  # def update
-  #   if current_user.update(user_params)
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # private
-
-  # def user_params
-  #   params.require(:user).permit(:name, :email)
-  # end
+  def set_group
+    @group = Group.find(params[:id])
+  end
 
 end
