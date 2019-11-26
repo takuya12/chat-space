8fbe389f9b3fba41255a77b36c282e0e0252580b
takuya12 <bucsojmfa12@gmail.com>
2019-11-26 17:29:01 +0900

    commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    # @group = Group.new(group_params)
-    # if @group.save
-    #   redirect_to root_path, notice: 'グループを作成しました'
-    # else
-    #   render :new
-    # end
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
-  # private
-  # def group_params
-  #   params.require(:group).permit(:name, user_ids: [] )
-  # end
+  private
 
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
