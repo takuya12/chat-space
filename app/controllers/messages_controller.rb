235fb36d018398ac93bf925ed828fce6410a5cf4
takuya12 <bucsojmfa12@gmail.com>
2019-11-26 15:37:05 +0900

    controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    # @group = Group.new(group_params)
+    # if @group.save
+    #   redirect_to root_path, notice: 'グループを作成しました'
+    # else
+    #   render :new
+    # end
+  end
+
+  # private
+  # def group_params
+  #   params.require(:group).permit(:name, user_ids: [] )
+  # end
+
+
 end
