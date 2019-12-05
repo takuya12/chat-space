e01425b8105efde5b05bb0eeab08bfe49371a2b8
takuya12 <bucsojmfa12@gmail.com>
2019-12-05 18:26:19 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -16,8 +18,11 @@ module ChatSpace
           g.helper false
           g.test_framework false
         end
+
+        config.i18n.default_locale = :ja
+        config.time_zone = 'Tokyo'
       end
     end
-    config.i18n.default_locale = :ja
+    
   end
 end
